dec3d55345c0850d87e974c730fa158b752585f0
Christian Sinai <christian_sinai@me.com>
2020-07-31 23:48:03 -0500

    testing pushed user selected parameter

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// create variables from arrays of possible alpha-numeric possibilities
+// create variables from arrays of possible alpha-numeric possibilities ...
 var specialChar = ["~","`","!","@","#","$","%","^","&","*","(",")","_","+","-","=","{","}","[","]",":",";","<",">","?",".","/","|"];
 var numbers = [0,1,2,3,4,5,6,7,8,9];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-// create variables to collect user alpha-numeric password parameters
+// create variables to collect user alpha-numeric password parameters ...
 var charCount = prompt("How many characters would you like your password to contain?");
 var confirmSpecChar = confirm("Click OK to confirm including special characters.");
 var confirmNumChar = confirm("Click OK to confirm including number characters.");
@@ -15,19 +15,25 @@ var confirmLowCaseChar = confirm("Click OK to confirm including lower case chara
 var confirmUpCaseChar = confirm("Click OK to confirm including upper case characters.");
 var masterArr = []
 
-// test logging array addition
-console.log(specialChar + numbers);
-
-// test code for logging random character
-var random = Math.floor(Math.random() * specialChar.length);
-console.log(specialChar[random]);
-
-// confirm user input is being accurately collected
+// confirm user input is being accurately collected ...
 // console.log(charCount);
 // console.log(confirmSpecChar);
 // console.log(confirmNumChar);
 // console.log(confirmLowCaseChar);
 // console.log(confirmUpCaseChar);
+
+// test code for logging random character ...
+// var random = Math.floor(Math.random() * specialChar.length);
+// console.log(specialChar[random]);
+
+// test logging array addition ...
+// console.log(specialChar + numbers);
+
+// test logging pushed user selected parameter ...
+if (confirmSpecChar) {
+  masterArr.push(specialChar);
+  console.log(masterArr[0][3]);
+}
 
 //// Write password to the #password input
 function writePassword() {
